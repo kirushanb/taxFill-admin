84dc6afb81a63730394ea945e684db51809295d9
Hansani Nadeera <nadeera@cyberozunu.com>
2022-09-23 17:00:55 +0530

    packge price updation

--- src/components/Dashboard/AddNew/PackageList.js
+++ src/components/Dashboard/AddNew/PackageList.js
@@ -1,37 +1,39 @@
 import * as React from 'react';
 import "./PackageList.scss";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer} from "react-toastify";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
-
-import {
-    Box,
-    Container,
-    TextField,
-
-} from "@mui/material";
+import { Box,Container} from "@mui/material";
+import PriceListItem from './PriceListItem';
 
 export default function PackageList() {
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
-
     const navigate = useNavigate();
+    const [packages, setPackages] = React.useState([]);
+    const axiosPrivate = useAxiosPrivate();
 
-    return (<div className="PackageList">
-        <form>
+    const getPackage = async () => {
+        try {
+            const response = await axiosPrivate.get("https://tax.api.cyberozunu.com/api/v1.1/Package");
+            setPackages(response.data?.result?.data);
+            console.log("data", response.data?.result?.data);
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    React.useEffect(() => {
+        getPackage();
+    }, []);
+
+
+    return (
+    <div className="PackageList">
+        <div className='form'>
             <ToastContainer />
             <Container component="main" maxWidth="lg">
                 <div className="back-button" onClick={() => navigate(-1)}>
@@ -41,48 +43,25 @@ export default function PackageList() {
                 <h3 className='title is-5'>Price of pakcages</h3>
 
                 <Box sx={{
-                    // marginTop: 8,
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "left",
                     width: "100%",
-                }}                >
-                    <Stack spacing={2}>
-                        <List sx={{ width: '50', textAlign: 'left', maxWidth: 480, bgcolor: 'background.paper' }}>
-                            {["Employment",
-                                "Self employment",
-                                "Pension Income",
-                                "Partnership",
-                                "Rental Income",
-                                "Dividend",
-                                "Bank interest",
-                            ].map((value) => (
-                                <Item className='item'>
-                                    <ListItem
-                                        key={value}
-                                        disableGutters
+                }}>
 
-                                    >
-                                        <ListItemText fullwidth primary={`${value} Package `} />
-                                        <TextField id="outlined-size-small"
-                                            sx={{
-                                                '& > :not(style)': { m: 1, width: '10ch' },
-                                            }}
-                                            label="price"
-                                            size="small"
-                                        />
-                                        <button className="button is-success"  >
-                                            Update
-                                        </button>
-                                    </ListItem>
-                                </Item>
+                    <Stack spacing={1}>
+                            {packages.map((item) => (
+                                <PriceListItem
+                                 item={item}
+                                 key ={item.id}
+                                 />
                             ))}
-                        </List>
+                            
                     </Stack>
 
                 </Box>
             </Container>
-        </form>
+    </div>
     </div>
     );
 }
